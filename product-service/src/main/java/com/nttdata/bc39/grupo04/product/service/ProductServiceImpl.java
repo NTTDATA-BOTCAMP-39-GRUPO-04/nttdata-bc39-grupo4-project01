1344572da0785c2f0aeb0781b294aadb921284d8
Nelson Jaimes Gonzales <njaimesg@emeal.nttdata.com>
2022-11-28 16:56:12 -0500

    update product-service

--- product-service/src/main/java/com/nttdata/bc39/grupo04/product/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/nttdata/bc39/grupo04/product/service/ProductServiceImpl.java
@@ -20,82 +20,82 @@ import reactor.core.publisher.Mono;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-	private ProductRepository repository;
-	private ProductMapper mapper;
-	private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+    private ProductRepository repository;
+    private ProductMapper mapper;
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
 
-	@Autowired
-	public ProductServiceImpl(ProductRepository repository, ProductMapper mapper) {
-		this.repository = repository;
-		this.mapper = mapper;
-	}
+    @Autowired
+    public ProductServiceImpl(ProductRepository repository, ProductMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
 
-	@Override
-	public Flux<ProductDTO> getAllProducts() {
-		return repository.findAll().map(mapper::entityToDto);
-	}
+    @Override
+    public Flux<ProductDTO> getAllProducts() {
+        return repository.findAll().map(mapper::entityToDto);
+    }
 
-	@Override
-	public Mono<ProductDTO> getProductByCode(String code) {
-		if (Objects.isNull(code)) {
-			LOG.info("Error, El Producto que intenta consultar es inválido");
-			throw new ServiceException("Error, El Producto que intenta consultar es invalido",
-					HttpStatus.NOT_FOUND.toString(), LocalDate.now().toString(), HttpStatus.NOT_FOUND.value());
-		}
-		
-		Mono<ProductDTO> productDTO = repository.findAll().filter(x -> x.getCode().equals(code)).next().map(mapper::entityToDto);	
-		if (Objects.isNull(productDTO.block())) {
-			LOG.info("Error, El Producto que intenta buscar no existe");
-			throw new ServiceException("Error, El Producto que intenta buscar no existe",
-					HttpStatus.NOT_FOUND.toString(), LocalDate.now().toString(), HttpStatus.NOT_FOUND.value());
-		}
-		return productDTO;
-	}
+    @Override
+    public Mono<ProductDTO> getProductByCode(String code) {
+        if (Objects.isNull(code)) {
+            LOG.info("Error, El Producto que intenta consultar es inválido");
+            throw new ServiceException("Error, El Producto que intenta consultar es invalido",
+                    HttpStatus.NOT_FOUND.toString(), LocalDate.now().toString(), HttpStatus.NOT_FOUND.value());
+        }
 
-	@Override
-	public Mono<ProductDTO> createProduct(ProductDTO dto) {
-		validateCreateProduct(dto);
-		ProductEntity entity = mapper.dtoToEntity(dto);
-		return repository.save(entity).map(mapper::entityToDto);
-	}
+        Mono<ProductDTO> productDTO = repository.findAll().filter(x -> x.getCode().equals(code)).next().map(mapper::entityToDto);
+        if (Objects.isNull(productDTO.block())) {
+            LOG.info("Error, El Producto que intenta buscar no existe");
+            throw new ServiceException("Error, El Producto que intenta buscar no existe",
+                    HttpStatus.NOT_FOUND.toString(), LocalDate.now().toString(), HttpStatus.NOT_FOUND.value());
+        }
+        return productDTO;
+    }
 
-	@Override
-	public Mono<ProductDTO> updateProduct(ProductDTO dto) {
-		// TODO Auto-generated method stub
-		Mono<ProductEntity> productEntity = repository.findAll().filter(x -> x.getCode().equals(dto.getCode())).next();
-		ProductEntity productEntityNew = productEntity.block();
-		
-		if (Objects.isNull(productEntityNew)) {
-			LOG.info("Error, El Producto que intenta modificar no existe");
-			throw new ServiceException("Error, El Producto que intenta modificar no existe",
-					HttpStatus.NOT_FOUND.toString(), LocalDate.now().toString(), HttpStatus.NOT_FOUND.value());
-		}
-		productEntityNew.setName(dto.getName());
-		productEntityNew.setTypeProduct(dto.getTypeProduct());
-		return repository.save(productEntityNew).map(mapper::entityToDto);
-	}
+    @Override
+    public Mono<ProductDTO> createProduct(ProductDTO dto) {
+        validateCreateProduct(dto);
+        ProductEntity entity = mapper.dtoToEntity(dto);
+        return repository.save(entity).map(mapper::entityToDto);
+    }
 
-	@Override
-	public Mono<Void> deleteProductByCode(String code) {
-		Mono<ProductEntity> productEntity = repository.findAll().filter(x -> x.getCode().equals(code)).next();
-		ProductEntity productEntityNew = productEntity.block();
-		if (Objects.isNull(productEntityNew)) {
-			LOG.info("Error, El Producto que intenta eliminar no existe");
-			throw new ServiceException("Error, El Producto que intenta eliminar no existe",
-					HttpStatus.NOT_FOUND.toString(), LocalDate.now().toString(), HttpStatus.NOT_FOUND.value());
-		}
-		return repository.delete(productEntityNew);
-	}
+    @Override
+    public Mono<ProductDTO> updateProduct(ProductDTO dto) {
+        // TODO Auto-generated method stub
+        Mono<ProductEntity> productEntity = repository.findAll().filter(x -> x.getCode().equals(dto.getCode())).next();
+        ProductEntity productEntityNew = productEntity.block();
 
-	private void validateCreateProduct(ProductDTO dto) {
-		Mono<ProductEntity> productEntity = repository.findAll().filter(x -> x.getCode().equals(dto.getCode())).next();
-		ProductEntity productEntityNew = productEntity.block();
-		
-		if (!Objects.isNull(productEntityNew)) {
-			LOG.info("Error, Ya existe un Producto registrado con el mismo código");
-			throw new ServiceException("Error, Ya existe un Producto registrado con el mismo código",
-					HttpStatus.NOT_FOUND.toString(), LocalDate.now().toString(), HttpStatus.NOT_FOUND.value());
-		}
-	}
+        if (Objects.isNull(productEntityNew)) {
+            LOG.info("Error, El Producto que intenta modificar no existe");
+            throw new ServiceException("Error, El Producto que intenta modificar no existe",
+                    HttpStatus.NOT_FOUND.toString(), LocalDate.now().toString(), HttpStatus.NOT_FOUND.value());
+        }
+        productEntityNew.setName(dto.getName());
+        productEntityNew.setTypeProduct(dto.getTypeProduct());
+        return repository.save(productEntityNew).map(mapper::entityToDto);
+    }
+
+    @Override
+    public Mono<Void> deleteProductByCode(String code) {
+        Mono<ProductEntity> productEntity = repository.findAll().filter(x -> x.getCode().equals(code)).next();
+        ProductEntity productEntityNew = productEntity.block();
+        if (Objects.isNull(productEntityNew)) {
+            LOG.info("Error, El Producto que intenta eliminar no existe");
+            throw new ServiceException("Error, El Producto que intenta eliminar no existe",
+                    HttpStatus.NOT_FOUND.toString(), LocalDate.now().toString(), HttpStatus.NOT_FOUND.value());
+        }
+        return repository.delete(productEntityNew);
+    }
+
+    private void validateCreateProduct(ProductDTO dto) {
+        Mono<ProductEntity> productEntity = repository.findAll().filter(x -> x.getCode().equals(dto.getCode())).next();
+        ProductEntity productEntityNew = productEntity.block();
+
+        if (!Objects.isNull(productEntityNew)) {
+            LOG.info("Error, Ya existe un Producto registrado con el mismo código");
+            throw new ServiceException("Error, Ya existe un Producto registrado con el mismo código",
+                    HttpStatus.NOT_FOUND.toString(), LocalDate.now().toString(), HttpStatus.NOT_FOUND.value());
+        }
+    }
 
 }
